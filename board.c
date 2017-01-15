7ae12d57aa6d5cb8beb33623417287b071cda81e
Anthony Liang <aliang8@stuy.edu>
2017-01-15 16:03:39 -0500

    starting to initialize the boards

---
+++ board.c
@@ -0,0 +1,37 @@
+#include "common.h"
+
+//create the board structure
+static struct board{
+  int height;
+  int width;
+  int **grid;
+  bool overflow; //checks if the board is filled
+  struct piece pc;
+  struct piece holdpc;
+} brd;
+
+void initbrd(int height, int width){
+  brd.height = height;
+  brd.width = width;
+  brd.overflow = false;
+  
+  //creating a height x width matrix of integers and initializing them at 0
+  int i,j;
+  brd.grid = malloc(brd.height * sizeof(int *)); //# of rows
+  for (i = 0; i < brd.height; i++){
+    brd.grid[i] = malloc(brd.width * sizeof(int)); //# of columns
+    for (j = 0; j < brd.width; j++)
+      brd.grid[i][j] = 0;
+  }
+}
+
+//free board memory
+void freebrd(){
+  if(brd.grid){
+    int i;
+    for (i = 0; i < brd.height; i++)
+      free(brd.grid[i]);
+    free(brd.grid);
+  }
+}
+  
---
+++ common.h
@@ -0,0 +1,16 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <ncurses.h>
+#include <panel.h>
+#include <time.h>
+#include <sys/time.h>
+
+enum colors {
+    WHITE = 1, BLACK, RED, GREEN, BLUE, CYAN, YELLOW, MAGENTA, ORANGE
+};
+
+#endif
