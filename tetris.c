9c7e101c9b9e8683a1ff30b2b2292e3cc90cfba6
Brian Yang <brian.yang328@gmail.com>
2017-01-18 15:50:28 -0500

    Work on displaying blocks

--- tetris.c
+++ tetris.c
@@ -4,15 +4,16 @@
 #define W_HEIGHT 20
 #define W_WIDTH  10
 
+static struct 
+
 void initcurses() {
-  initscr(); //start curses mode  no
-  noecho();
+  initscr(); //start curses mode
+  noecho(); //don't echo keypresses
   cbreak();
   halfdelay(5);
-  curs_set(false);
+  curs_set(false); //don't display cursor
   keypad(stdscr, true);
   srand(time(NULL));
-  refresh();
 }
 
 WINDOW *create_board(int height, int width, int starty, int startx){
@@ -31,9 +32,26 @@ int main(){
   starty = (LINES - W_HEIGHT)/2;
   startx = (COLS - W_WIDTH)/2 ;
   
-  board = create_board(W_HEIGHT, W_WIDTH, starty, startx);
+  board = create_board(W_HEIGHT, W_WIDTH, starty, startx);  
+  start_color();
+  init_pair(1,COLOR_GREEN,COLOR_GREEN);
+  wattron(board, COLOR_PAIR(1)); 
+  mvwprintw(board,1,1,"xx"); 
+  getch();
   while(1){
+    wrefresh(board);
   };
+  
+  /*int x = 0, y = 0;
+  initcurses();
+  while(1){
+    clear();
+    mvprintw(y,x,"o");
+    refresh();
+    usleep(3000);
+    x++;
+  };
+  */
   endwin();
   return 0;
 }
